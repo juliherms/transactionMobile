fca3012b3c2b56df72ec8c4e4a918e92bb09407d
Juliherms Vasconcelos <37042672+juliherms@users.noreply.github.com>
2020-08-08 12:19:02 -0300

    create login screen

--- app/src/main/java/com/example/transactionmobile/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/transactionmobile/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.transactionmobile.R
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -28,5 +29,15 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view,savedInstanceState)
+
+        //redirect to login screen
+        val dir = HomeFragmentDirections.actionNavigationHomeToLoginFragment()
+        val controller = findNavController()
+        controller.navigate(dir)
+
     }
 }
